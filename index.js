2cd3d099f38f9710924836d190790db4bd360c96
Nicolas Wormser <nicolas@cybercrumbs.net>
2016-11-30 16:39:23 +0400

    Add simple timer (#5)
    * Add simple timer
    * Use setTimeout with params instead of bind

--- index.js
+++ index.js
@@ -5,20 +5,31 @@
  */
 const horloge = require('commander');
 
-const startTimer = function (opts) {
-  if (opts.breakDuration) {
-    console.log('Started with --break_duration %s', opts.breakDuration);
-  }
-  if (opts.workDuration) {
-    console.log('Started with --work_duration %s', opts.workDuration);
+/**
+ * Methods
+ */
+const runInterval = function (duration, interval = 60000) {
+  console.log(`${duration}mn left`);
+  if (duration === 0) {
+    console.log("Time's up");
+  } else {
+    // calls runInterval after interval is elapsed with the updated parameters
+    setTimeout(runInterval, interval, duration - 1, interval);
   }
 };
 
+/**
+ * Interface
+ */
+const controls = {
+  start(duration) {
+    runInterval(duration);
+  },
+};
+
 horloge
-  .command('start')
-  .description('start the Horloge timer.')
-  .option('-b, --break-duration <duration>', 'Time in minutes for break sessions')
-  .option('-w, --work-duration <duration>', 'Time in minutes for work sessions')
-  .action(startTimer);
+  .command('start <duration>')
+  .description('starts the horloge timer with a duration in minutes.')
+  .action(controls.start);
 
 horloge.parse(process.argv);
